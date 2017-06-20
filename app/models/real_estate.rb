f2caaca99743f63dbf4a484816fcf09fb7c0ec45
prio101 <miprio101@gmail.com>
2017-06-21 04:46:18 +0600

    Added the Validation and create button

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,2 +1,8 @@
 class RealEstate < ApplicationRecord
+  validates :street, :city, :zip, :state, :beds,
+            :baths, :sq_ft, :type_of_estate,
+            :sale_date, :price,
+            :latitude, :longitude, presence: true
+  validates :zip, :price, :latitude,
+            :longitude, :sq_ft, numericality: true
 end
